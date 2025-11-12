972bf7867f6d7462c3872a35d048fc75a6d3cbe7
GO0QN <109978915+GO0QN@users.noreply.github.com>
2025-11-12 03:35:33 -0500

    Update firebase-init.js

--- js/firebase-init.js
+++ js/firebase-init.js
@@ -1,10 +1,8 @@
-// Central Firebase init using YOUR config
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
 import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
 
-// Your config (the one you pasted)
-const firebaseConfig = {
+export const app = initializeApp({
   apiKey: "AIzaSyCe4NxqivGSKDI3aHvJrU88bPOxzSYLh_Y",
   authDomain: "osu-daytraders.firebaseapp.com",
   projectId: "osu-daytraders",
@@ -12,9 +10,7 @@ const firebaseConfig = {
   messagingSenderId: "63619855631",
   appId: "1:63619855631:web:3416fee03d357715f0fa7a",
   measurementId: "G-RXTL84LM0T"
-};
-
-export const app  = initializeApp(firebaseConfig);
+});
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
-export const db   = getFirestore(app);
+export const db = getFirestore(app);
