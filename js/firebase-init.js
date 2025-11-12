b0d3ccf9e1834454f30c8c79b0f0ee2fa1ac0469
GO0QN <109978915+GO0QN@users.noreply.github.com>
2025-11-12 04:42:30 -0500

    Update firebase-init.js

--- js/firebase-init.js
+++ js/firebase-init.js
@@ -1,16 +1,23 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
-import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
+<!-- /js/firebase-init.js -->
+<script type="module">
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
+  import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-analytics.js";
 
-export const app = initializeApp({
-  apiKey: "AIzaSyCe4NxqivGSKDI3aHvJrU88bPOxzSYLh_Y",
-  authDomain: "osu-daytraders.firebaseapp.com",
-  projectId: "osu-daytraders",
-  storageBucket: "osu-daytraders.firebasestorage.app",
-  messagingSenderId: "63619855631",
-  appId: "1:63619855631:web:3416fee03d357715f0fa7a",
-  measurementId: "G-RXTL84LM0T"
-});
-export const auth = getAuth(app);
-export const provider = new GoogleAuthProvider();
-export const db = getFirestore(app);
+  const firebaseConfig = {
+    apiKey: "AIzaSyCe4NxqivGSKDI3aHvJrU88bPOxzSYLh_Y",
+    authDomain: "osu-daytraders.firebaseapp.com",
+    projectId: "osu-daytraders",
+    storageBucket: "osu-daytraders.firebasestorage.app",
+    messagingSenderId: "63619855631",
+    appId: "1:63619855631:web:3416fee03d357715f0fa7a",
+    measurementId: "G-RXTL84LM0T"
+  };
+
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth(app);
+  getAnalytics(app);
+
+  // Expose for other modules
+  window.__firebase = { app, auth };
+</script>
